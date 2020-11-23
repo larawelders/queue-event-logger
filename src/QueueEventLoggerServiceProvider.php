c8bb3faf9471078deddc536357229e2456f1b235
iamgergo <hello@iamgergo.com>
2020-11-23 13:15:54 +0100

    tests

--- src/QueueEventLoggerServiceProvider.php
+++ src/QueueEventLoggerServiceProvider.php
@@ -2,20 +2,18 @@
 
 namespace Larawelders\QueueEventLogger;
 
+use Illuminate\Queue\Events\JobExceptionOccurred;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Queue\Events\Looping;
+use Illuminate\Queue\Events\WorkerStopping;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class QueueEventLoggerServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Boot any application services.
      *
@@ -23,6 +21,17 @@ class QueueEventLoggerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(JobExceptionOccurred::class, static function (JobExceptionOccurred $event) {
+            Log::channel('queue')->error(
+                sprintf(
+                    '[%s] Uncaught exception %s in job %s: %s%s',
+                    $event->job->getJobId(),
+                    get_class($event->exception),
+                    $event->job->resolveName(),
+                    $event->exception->getMessage(),
+                    property_exists($event->job, 'contextString') ? ', '.$event->job->contextString : ''
+                )
+            );
+        });
     }
 }
--- tests/Feature/QueueEventLoggerTest.php
+++ tests/Feature/QueueEventLoggerTest.php
@@ -2,13 +2,39 @@
 
 namespace Larawelders\QueueEventLogger\Tests\Feature;
 
+use Exception;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\Events\JobExceptionOccurred;
+use Illuminate\Support\Facades\Log;
 use Larawelders\QueueEventLogger\Tests\TestCase;
 
 class QueueEventLoggerTest extends TestCase
 {
     /** @test */
-    public function it_logs_queue_events()
+    public function it_logs_job_exception_occurred_event()
     {
-        $this->assertTrue(true);
+        $job = new TestJob;
+        $exception = new Exception('TestJob failed');
+
+        $this->app['events']->dispatch(
+            new JobExceptionOccurred('default', $job, $exception)
+        );
+
+        Log::shouldReceive('queue')->with(
+            '[test-job] Uncaught exception Exception in job Larawelders\\QueueEventLogger\\Tests\\Feature\\TestJob: TestJob failed',
+        );
+    }
+}
+
+class TestJob implements ShouldQueue
+{
+    public function resolveName()
+    {
+        return static::class;
+    }
+
+    public function getJobId()
+    {
+        return 'test-job';
     }
 }
